e61e28ab96e1d4b3af1f03cdf3777a931709caf2
Yuxuan Shui <yshuiv7@gmail.com>
2018-07-19 21:37:51 +0000

    Remove conky configs

--- .conky/conky.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/dash -f 
-wmiir read /rbar/conky > /dev/null
-if [ $? -eq 0 ]; then
-       wmiir remove /rbar/conky
-       sleep 2
-fi       
-echo '' | wmiir create /rbar/conky
-conky-cli -c ~/.conky-clirc | ~/.conky/conky_write.pl
-
--- .conky/conky_write.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-while(<>){
-	chomp;
-	$u = quotemeta($_);
-	`wmiir xwrite /rbar/conky "label" $u`;
-	last if $?;
-}
-`killall conky-cli`;
-`wmiir remove /rbar/conky`;
