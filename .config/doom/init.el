b3c7cd54b2ca837aac4ecc08e5d3778080562ebd
Yuxuan Shui <yshuiv7@gmail.com>
2018-12-28 09:29:23 +0000

    Add parinfer

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -46,7 +46,7 @@
        ;;(format +onsave)  ; automated prettiness
        ;;lispy             ; vim for lisp, for people who dont like vim
        multiple-cursors  ; editing in many places at once
-       ;;parinfer          ; turn lisp into python, sort of
+       parinfer          ; turn lisp into python, sort of
        rotate-text       ; cycle region at point between text candidates
 
        :emacs
