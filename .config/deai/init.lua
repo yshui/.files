4f3cad7f686f75c3ea9fe0780d9777542d676fbe
Yuxuan Shui <yshuiv7@gmail.com>
2020-12-04 19:28:53 +0000

    update deai init.lua

--- .config/deai/init.lua
+++ .config/deai/init.lua
@@ -23,9 +23,11 @@ di.os.env.RADV_PERFTEST = "cswave32,gewave32"
 di.os.env.XDG_CURRENT_DESKTOP = "MATE"
 
 di.os.env.MOZ_USE_XINPUT2 = "1"
+di.os.env.MOZ_X11_EGL = "1"
 
 -- Make ubsan stricter
 di.os.env.UBSAN_OPTIONS="halt_on_error=1:print_stacktrace=1"
+di.os.env.SSH_ASKPASS="/usr/lib/seahorse/ssh-askpass"
 
 -- get the config directory, used for putting the log file
 if di.os.env["XDG_CONFIG_HOME"] ~= nil then
@@ -41,9 +43,9 @@ if di.os.env.DEAI_RESTARTED ~= nil then
 end
 
 di.log.log_level = "info"
-di.log.log_target = di.log.file_target(cfg_dir.."/log.txt", false)
+di.log.log_target = di.log:file_target(cfg_dir.."/log.txt", false)
 -- connect to xorg
-local xc = di.xorg.connect()
+local xc = di.xorg:connect()
 
 function get_output(name)
     local os = xc.randr.outputs
@@ -57,16 +59,16 @@ end
 -- alternatively:
 -- xc = di.xorg.connect_to(<display>)
 
-xc.on("connection-error", function()
+xc:on("connection-error", function()
     -- Quit when xorg connection dies
-    di.quit()
+    di:quit()
 end)
 
 -- set this shortcut early, in case the rest of the script failed to load
-xc.key.new({"mod4"}, "r", false).on("pressed", function()
-    di.log("warn", "deai restarting...")
+xc.key:new({"mod4"}, "r", false):on("pressed", function()
+    di:log("warn", "deai restarting...")
     di.os.env.DEAI_RESTARTED = "1"
-    di.exec(di.argv)
+    di:exec(di.argv)
 end)
 
 -- Apply settings to xinput device
@@ -74,7 +76,7 @@ end)
 function apply_xi_settings(dev)
     local p = dev.props
     if dev.type == "touchpad" then
-        p["libinput Tapping Enabled"] = {1}
+        p["libinput Tapping Enabled"] = 1
         p["libinput Tapping Button Mapping Enabled"] = {0,1}
     end
 
@@ -82,20 +84,37 @@ function apply_xi_settings(dev)
         if string.find(dev.name, "ELECOM") or string.find(dev.name, "Logitech") or string.find(dev.name, "MX Ergo") then
             p["libinput Scroll Method Enabled"] = {0, 0, 1}
             if string.find(dev.name, "ELECOM") then
-                p["libinput Button Scrolling Button"] = {10}
+                -- some ELECOM mouses have the same names, we check the device
+                -- ids here
+                local dev_node = p["Device Node"]
+                print(dev_node)
+                local model_id
+                if dev_node ~= nil then
+                    local dev = di.evdev:open(dev_node)
+                    if dev ~= nil and dev.id ~= nil then
+                        model_id = dev.id.product
+                    end
+                end
+                if model_id == 0x00fe then
+                    -- this is my Deft
+                    p["libinput Button Scrolling Button"] = 9
+                else
+                    -- unknown, assuming it's my Ex-G
+                    p["libinput Button Scrolling Button"] = 10
+                end
             else
-                p["libinput Button Scrolling Button"] = {9}
+                p["libinput Button Scrolling Button"] = 9
             end
         end
-        p["libinput Natural Scrolling Enabled"] = {1}
+        p["libinput Natural Scrolling Enabled"] = 1
     end
 end
 
 local xi = xc.xinput
 
 -- add listener for new-device event
-xi.on("new-device", function(dev)
-    di.log("info", string.format("new device %s %s %s %d", dev.type, dev.use, dev.name, dev.id))
+xi:on("new-device", function(dev)
+    di:log("info", string.format("new device %s %s %s %d", dev.type, dev.use, dev.name, dev.id))
     -- apply settings to new device
     apply_xi_settings(dev)
     -- set keymap
@@ -107,17 +126,17 @@ xc.keymap = { layout = "us", options = "ctrl:nocaps" }
 -- apply settings to existing devices
 local devs = xi.devices;
 for i, v in ipairs(devs) do
-    di.log("info", string.format("%d %d %s %s %s", i, v.id, v.name, v.type, v.use))
+    di:log("info", string.format("%d %d %s %s %s", i, v.id, v.name, v.type, v.use))
     apply_xi_settings(v)
 end
 
 local function replace_awesome()
-    local awesome = di.spawn.run({"awesome", "--replace"}, false)
-    awesome.on("stderr_line", function(l)
-        di.log("warn", "[awesome.ERR] "..l)
+    local awesome = di.spawn:run({"awesome", "--replace"}, false)
+    awesome:on("stderr_line", function(l)
+        di:log("warn", "[awesome.ERR] "..l)
     end)
-    awesome.on("stdout_line", function(l)
-        di.log("info", "[awesome.OUT] "..l)
+    awesome:on("stdout_line", function(l)
+        di:log("info", "[awesome.OUT] "..l)
     end)
 end
 
@@ -129,25 +148,26 @@ if not restarted then
     local function auto_start()
         replace_awesome()
 
-        di.spawn.run({"picom", "--experimental-backends"}, true)
-        di.spawn.run({"fcitx"}, true)
+        di.spawn:run({"picom", "--experimental-backends"}, true)
+        di.spawn:run({"fcitx"}, true)
         --di.spawn.run({"transmission-daemon"}, true)
-        di.spawn.run({"synergys", "--name", "pc"}, true)
-        di.spawn.run({"mpd"}, true)
-        di.spawn.run({"syncthing", "-no-browser"}, true)
-        di.spawn.run({"syncthing-inotify"}, true)
+        di.spawn:run({"synergys", "--name", "pc"}, true)
+        di.spawn:run({"mpd"}, true)
+        di.spawn:run({"syncthing", "-no-browser"}, true)
+        di.spawn:run({"syncthing-inotify"}, true)
         --di.spawn.run({"jack_control", "start"}, true)
-        di.spawn.run({"emacs", "--daemon"}, true)
+        di.spawn:run({"emacs", "--daemon"}, true)
+        di.spawn:run({"/usr/lib/mate-polkit/polkit-mate-authentication-agent-1"}, true)
     end
     -- test if a dbus daemon is already running and reachable
     local bus = di.dbus.session_bus
     if bus == nil or bus.errmsg then
-        local dbusl = di.spawn.run({"dbus-launch"}, false)
+        local dbusl = di.spawn:run({"dbus-launch"}, false)
         --run auto start only after dbus-launch finishes
-        dbusl.on("exit", function()
+        dbusl:on("exit", function()
             auto_start()
         end)
-        dbusl.on("stdout_line", function(l)
+        dbusl:on("stdout_line", function(l)
             e = string.find(l, "=")
             if e == nil then
                 return
@@ -160,50 +180,54 @@ if not restarted then
     bus = nil
 end
 
-xc.key.new({"mod4", "control"}, "r", false).on("pressed", function()
-    di.log("info", "restarting awesome...")
+xc.key:new({"mod4", "control"}, "r", false):on("pressed", function()
+    di:log("info", "restarting awesome...")
     replace_awesome()
 end)
 
-xc.key.new({"mod4", "shift"}, "e", false).on("pressed", function()
-    di.log("info", "deai quitting...")
-    di.terminate()
+xc.key:new({"mod4", "shift"}, "e", false):on("pressed", function()
+    di:log("info", "deai quitting...")
+    di:terminate()
 end)
 
-xc.key.new({"mod4", "shift"}, "l", false).on("pressed", function()
-    di.spawn.run({"i3lock"}, true)
-    kpass = di.dbus.session_bus.get("org.freedesktop.secrets", "/keepassxc")
+xc.key:new({"mod4", "shift"}, "l", false):on("pressed", function()
+    di.spawn:run({"i3lock"}, true)
+    kpass = di.dbus.session_bus:get("org.freedesktop.secrets", "/keepassxc")
     kpass["org.keepassxc.MainWindow.lockAllDatabases"]()
 end)
 
-xc.key.new({"mod4"}, "d", false).on("pressed", function()
-    di.spawn.run({"rofi", "-show", "run"}, true)
+xc.key:new({"mod4"}, "d", false):on("pressed", function()
+    di.spawn:run({"rofi", "-show", "run"}, true)
 end)
 
-xc.key.new({"mod4"}, "z", false).on("pressed", function()
-    di.spawn.run({"mpc", "toggle"}, true)
+xc.key:new({"mod4"}, "z", false):on("pressed", function()
+    di.spawn:run({"mpc", "toggle"}, true)
 end)
 
-xc.key.new({"mod4"}, "x", false).on("pressed", function()
-    di.spawn.run({"mpc", "prev"}, true)
+xc.key:new({"mod4"}, "x", false):on("pressed", function()
+    di.spawn:run({"mpc", "prev"}, true)
 end)
 
-xc.key.new({"mod4"}, "c", false).on("pressed", function()
-    di.spawn.run({"mpc", "next"}, true)
+xc.key:new({"mod4"}, "c", false):on("pressed", function()
+    di.spawn:run({"mpc", "next"}, true)
 end)
 
-xc.key.new({}, "XF86AudioPlay", false).on("pressed", function()
-    di.spawn.run({"mpc", "toggle"}, true)
+xc.key:new({"mod4"}, "w", false):on("pressed", function()
+    di.spawn:run({"wimp"}, true)
 end)
 
-xc.key.new({}, "XF86AudioPause", false).on("pressed", function()
-    di.spawn.run({"mpc", "pause"}, true)
+xc.key:new({}, "XF86AudioPlay", false):on("pressed", function()
+    di.spawn:run({"mpc", "toggle"}, true)
 end)
 
-xc.key.new({}, "XF86AudioPrev", false).on("pressed", function()
-    di.spawn.run({"mpc", "prev"}, true)
+xc.key:new({}, "XF86AudioPause", false):on("pressed", function()
+    di.spawn:run({"mpc", "pause"}, true)
 end)
 
-xc.key.new({}, "XF86AudioNext", false).on("pressed", function()
-    di.spawn.run({"mpc", "next"}, true)
+xc.key:new({}, "XF86AudioPrev", false):on("pressed", function()
+    di.spawn:run({"mpc", "prev"}, true)
+end)
+
+xc.key:new({}, "XF86AudioNext", false):on("pressed", function()
+    di.spawn:run({"mpc", "next"}, true)
 end)
